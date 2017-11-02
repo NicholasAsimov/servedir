4dd2aaf83bfea47f9230aca8d9860adf1b33ceb9
Nicholas Asimov <nicholas@asimov.me>
2017-11-02 20:08:04 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	var (
+		host string
+		port int
+		dir  string
+		ssl  bool
+		cert string
+		key  string
+	)
+
+	flag.StringVar(&host, "host", "localhost", "host to listen on")
+	flag.IntVar(&port, "port", 433, "port to listen on")
+	flag.StringVar(&dir, "dir", ".", "directory to serve")
+	flag.BoolVar(&ssl, "ssl", false, "use ssl")
+	flag.StringVar(&cert, "cert", "", "path to ssl certificate, requires -ssl")
+	flag.StringVar(&key, "key", "", "path to ssl key, requires -ssl")
+
+	flag.Parse()
+
+	if ssl && (cert == "" || key == "") {
+		log.Println("Error: Specify ssl cert and key paths")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if !ssl && (cert != "" || key != "") {
+		log.Println("Error: Using -cert or -key requires -ssl")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	http.Handle("/", http.FileServer(http.Dir(dir)))
+
+	if ssl {
+		log.Fatal(http.ListenAndServeTLS(fmt.Sprintf("%s:%d", host, port), cert, key, nil))
+	} else {
+		log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), nil))
+	}
+}
